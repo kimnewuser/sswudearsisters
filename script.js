81d11898fddb08bab90e07a952e20b13a2166d62
김뉴비 <151442617+kimnewuser@users.noreply.github.com>
2025-05-28 05:58:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,7 @@ window.addEventListener('scroll', function () {
     const posterTop = poster.offsetTop;
     const posterBottom = posterTop + poster.offsetHeight;
     const scrollY = window.scrollY;
-  
-    // 포스터가 완전히 스크롤되어 올라가면 opacity 0
-    if (scrollY >= posterBottom) {
-      poster.style.opacity = 0;
-    } else {
-      const ratio = 1 - scrollY / posterBottom;
-      poster.style.opacity = 0.5 * ratio;
-    }
+
   });
 
 const menuButton = document.getElementById('menuButton');
