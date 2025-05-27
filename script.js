64d5b149a9e6a2581d5474d1c7132e839cdb3578
김뉴비 <151442617+kimnewuser@users.noreply.github.com>
2025-05-28 05:59:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-window.addEventListener('scroll', function () {
-    const poster = document.getElementById('poster');
-    const posterTop = poster.offsetTop;
-    const posterBottom = posterTop + poster.offsetHeight;
-    const scrollY = window.scrollY;
-
-  });
 
 const menuButton = document.getElementById('menuButton');
 const sideMenu = document.getElementById('sideMenu');
